412014a48a57bf4340ed404c47246ebd5c4ef2f6
gothness <gothness@ymail.com>
2018-04-23 15:08:14 +0300

    Change struct

--- pymt5/mt5_group.py
+++ pymt5/mt5_group.py
@@ -51,14 +51,55 @@ class MT5Group(object):
 
         return json.loads(body.data)
 
-    def add(self):
-        pass
+    def add(self, data):
+        """
+        Add group
+        :param data:
+        :return:
+        """
+        if not self.connect.send(self.CMD_GROUP_ADD, {}, data=data):
+            self.logger.error("Add group failed")
+            return False
+
+        try:
+            header, body = self.connect.read()
+        except TypeError:
+            self.logger.error("Add group failed")
+            return False
+
+        if MT5ReturnCodes.PARAM not in body.options \
+                or body.data is None \
+                or body.options[MT5ReturnCodes.PARAM] != MT5ReturnCodes.STATUS_DONE:
+            self.logger.error("Add group failed")
+            return False
+
+        return json.loads(body.data)
 
     def delete(self):
         pass
 
     def get_total(self):
-        pass
+        """
+        Get total group
+        :return:
+        """
+        if not self.connect.send(self.CMD_GROUP_TOTAL, {}):
+            self.logger.error("Get group total failed")
+            return False
+
+        try:
+            header, body = self.connect.read()
+        except TypeError:
+            self.logger.error("Get group total failed")
+            return False
+
+        if MT5ReturnCodes.PARAM not in body.options \
+                or body.data is None \
+                or body.options[MT5ReturnCodes.PARAM] != MT5ReturnCodes.STATUS_DONE:
+            self.logger.error("Get group failed")
+            return False
+
+        return json.loads(body.data)
 
     def get_next(self):
         pass
